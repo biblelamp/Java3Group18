9b4d6bfd9d5f81b93bedc7eb865cb5387cbc54ca
rgajdov <rgajdov@gmail.com>
2018-07-14 22:43:01 +0300

    All

--- lesson1/Apple.java
+++
@@ -1,8 +0,0 @@
-package ru.rgajdov.java3.lesson1;
-
-public class Apple extends Fruit {
-
-    public Apple() {
-        this.weight = 1.0f;
-    }
-}
--- lesson1/VirtualArray.java
+++
@@ -1,9 +0,0 @@
-package ru.rgajdov.java3.lesson1;
-
-import java.util.ArrayList;
-
-public interface VirtualArray<E> {
-    void swapElements(E[] array, int index1, int index2);
-    ArrayList<E> arrayToArrayList(E[] array);
-    String display(E[] array);
-}
