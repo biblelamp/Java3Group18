7c07fa7a8a42913638a6a85a5fd568f48f24d4da
Mikhail <mbarino@yandex.ru>
2018-07-30 14:53:29 +0300

    Добавил ДЗ №7

---
+++ Mikhail Barinov Java Level 3/Homework_07/TestedClass.java
@@ -0,0 +1,29 @@
+package Homework_07;
+
+import java.lang.reflect.Method;
+
+public class TestedClass {
+    @BeforeSuite
+    public void myHelloMethod(){
+        System.out.println("Hello!");
+    }
+    @Test
+    void myMainMethod1(){
+        System.out.println("Do something 1");
+    }
+    @Test (priorityValue = 10)
+    void myMainMethod2(){
+        System.out.println("Do something 2");
+    }
+    void mySilentMethod(){
+        System.out.println("This method should not be tested!");
+    }
+    @AfterSuite
+    void myGoodbyeMethod(){
+        System.out.println("Bye!");
+    }
+//    @BeforeSuite
+//    public void myFailMethod(){
+//        System.out.println("Fail!");
+//    }
+}
